9f6ad6dfb9ef1a4044e8b44db6a33388ff352770
IvensAr7 <ivens.silva1@aluno.ce.gov.br>
2025-10-21 22:25:44 -0300

    Adicionado biografia na home + slideshow automático

--- script_home.js
+++ script_home.js
@@ -1,20 +1,3 @@
-// slideshow
-
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
-}
-
 // navbar background change on scroll
 
 const navbar = document.getElementById('navbar');
